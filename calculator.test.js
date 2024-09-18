066a595edaae0c2469bd155eb5ef4b86909e1ddc
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-18 15:50:59 -0600

    feat(calculator): Make add func work via TDD

---
+++ calculator.test.js
@@ -0,0 +1,24 @@
+import { calculator } from "./calculator.js";
+
+test("Calculator exists", () => {
+  expect(calculator).toBeDefined();
+});
+
+test("add, subtract, multiply, divide funcs exist", () => {
+  expect(calculator.add).toBeDefined();
+  expect(calculator.subtract).toBeDefined();
+  expect(calculator.multiply).toBeDefined();
+  expect(calculator.divide).toBeDefined();
+});
+
+test("add function works (ex-1)", () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
+
+test("add function works (ex-2)", () => {
+  expect(calculator.add(54, 37)).toBe(91);
+});
+
+test("add function works (ex-3)", () => {
+  expect(calculator.add(643, 999)).toBe(1642);
+});
