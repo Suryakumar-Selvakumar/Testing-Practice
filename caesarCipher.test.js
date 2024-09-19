3dba0b1bdc5b726630fc3d933802e2930accd228
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-18 19:43:08 -0600

    fix(caesarCipher): Change toMatch() to toEqual()
    Change the function toMatch() to toEqual() to check for strict equality
    of expected and recieved strings.

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -5,30 +5,29 @@ test("caesarCipher exists", () => {
 });
 
 test("caesarCipher returns a string", () => {
-  expect(caesarCipher("str")).toMatch(/[\s\S]*/);
+  expect(caesarCipher("str", 2)).toMatch(/[a-zA-Z]*/);
 });
 
 test("caesarCipher returns the shifted string (ex-1)", () => {
-  expect(caesarCipher("def", 3)).toMatch("ghi");
+  expect(caesarCipher("def", 3)).toEqual("ghi");
 });
 
 test("caesarCipher returns the shifted string (ex-2)", () => {
-  expect(caesarCipher("str", 2)).toMatch("uvt");
+  expect(caesarCipher("str", 2)).toEqual("uvt");
 });
 
 test("caesarCipher returns the shifted string (ex-3)", () => {
-  expect(caesarCipher("xyz", 3)).toMatch("abc");
+  expect(caesarCipher("xyz", 3)).toEqual("abc");
 });
 
 test("caesarCipher returns the shifted string with the same case", () => {
-  expect(caesarCipher("HeLLo", 3)).toMatch("KhOOr");
+  expect(caesarCipher("HeLLo", 3)).toEqual("KhOOr");
 });
 
 test("caesarCipher returns the shifted string with punctuation (ex-1)", () => {
-  expect(caesarCipher('Hello, World!', 3)).toMatch("Khoor, Zruog!");
+  expect(caesarCipher("Hello, World!", 3)).toEqual("Khoor, Zruog!");
 });
 
 test("caesarCipher returns the shifted string with punctuation (ex-2)", () => {
-  expect(caesarCipher('Hello,, World!', 3)).toMatch("Khoor,, Zruog!");
+  expect(caesarCipher("Hello,, World!", 3)).toEqual("Khoor,, Zruog!");
 });
-
